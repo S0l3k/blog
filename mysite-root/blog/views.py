6f8d5aacbb63e237c298d2dc3aed14a571c709a8
S0l3k <turtleteamwcg@gmail.com>
2023-11-10 14:48:13 +0300

    создал разбивку по страницам и шаблон для пагинации

--- mysite-root/blog/views.py
+++ mysite-root/blog/views.py
@@ -1,15 +1,14 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     # Постраничная разбивка страницы по 3 поста на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    post = paginator.page(page_number)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
