8c9162085feb78fe4be5d242251297fa9e3a1a29
S0l3k <turtleteamwcg@gmail.com>
2023-11-29 10:16:10 +0300

    добавил комментарии в представление детальной информации о посте

--- mysite-root/blog/views.py
+++ mysite-root/blog/views.py
@@ -29,8 +29,13 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев к посту
+    comments = post.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'form': form})
 
 
 class PostListView(ListView):
