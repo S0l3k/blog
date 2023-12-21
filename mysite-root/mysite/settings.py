a8afaf46dff6e09a22dc2e89d709d5d26ad7aa67
S0l3k <turtleteamwcg@gmail.com>
2023-12-21 20:02:51 +0300

    создал карту сайта

--- mysite-root/mysite/settings.py
+++ mysite-root/mysite/settings.py
@@ -28,6 +28,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
 
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
